3d647efb222e4020d008eba4bbedcb4306be2aba
Zunair Shahzad <zunairshahzad391@gmail.com>
2026-04-11 17:16:32 +0500

    Reset: Starting project from scratch

--- components/footer.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def show_footer():
-    st.markdown("---")
-    col1, col2, col3 = st.columns(3)
-    with col2:
-        st.caption("Made with ❤️ using Streamlit | Version 2.0")
